ff6afe1b83727c2473e24f859b23c612453c3020
Rogier Bosschaart <rogierbosschaart@gmail.com>
2025-05-21 16:45:59 +0200

    setup and start of styling

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Simple Form
+gem "simple_form"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
@@ -57,3 +60,5 @@ group :development do
   # gem "spring"
 end
 
+gem 'rspec-rails', group: [ :test ]
+gem 'rails-controller-testing', group: [ :test ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Clearing Restaurants"
+Restaurant.destroy_all
+
+puts "Creating Restaurants"
+
+Restaurant.create!(name: 'Toast', address: 'Toast Ave. Italy', phone_number: '+31 17 24 252525', category: 'italian')
+Restaurant.create!(name: 'Duckling', address: '204 China Street, NYC', phone_number: '+31 17 36 171717', category: 'chinese')
+Restaurant.create!(name: 'Looper', address: 'Cereal Solution St. Netherlands', phone_number: '+31 17 11 87161', category: 'japanese')
+Restaurant.create!(name: 'Cracker Barrel', address: 'Alabama', phone_number: '+31 17 90 92891', category:'french')
+Restaurant.create!(name: 'Burger Beast', address: 'The Hague', phone_number: '+31 17 24 21121', category: 'belgian')
+
+puts "Created Restaurants"
